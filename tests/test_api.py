ac5a30255fefc0149c337abc99344844b47a9fb3
anna-zukowska <anna-zukowska@ya.ru>
2022-12-18 03:05:13 +0500

    refactored tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,8 @@ from reqres_tests.responses.auth import AuthSuccessful, AuthUnSuccessful
 from reqres_tests.responses.users import CreatedUser, UpdatedUser, DeletedUser
 
 load_dotenv()
-LOGIN = os.getenv('user_login')
-PASSWORD = os.getenv('user_password')
+login = os.getenv('user_login')
+password = os.getenv('user_password')
 
 
 @allure.tag('critical')
@@ -18,7 +18,7 @@ PASSWORD = os.getenv('user_password')
 @allure.story('Авторизация пользователя')
 @allure.title('Успешная авторизация')
 def test_login_successful():
-    result = Reqres().login(LOGIN, PASSWORD)
+    result = Reqres().login(login, password)
     AuthSuccessful(result).assert_()
 
 
@@ -28,7 +28,7 @@ def test_login_successful():
 @allure.story('Авторизация пользователя')
 @allure.title('Неуспешная авторизация (не указан пароль)')
 def test_login_unsuccessful():
-    result = Reqres().login(LOGIN, '')
+    result = Reqres().login(login, '')
     AuthUnSuccessful(result).assert_()
 
 
@@ -39,7 +39,7 @@ def test_login_unsuccessful():
 @allure.title('Добавление нового пользователя')
 @pytest.mark.parametrize(['name', 'job'], [('Anna Zukowska', 'QA'), ('Anna', '')])
 def test_create_user(name, job):
-    token = Reqres().get_token(LOGIN, PASSWORD)
+    token = Reqres().get_token(login, password)
     result = Reqres().create_user(name=name, job=job, token=token)
     CreatedUser(result).assert_(name, job)
 
@@ -50,7 +50,7 @@ def test_create_user(name, job):
 @allure.story('Действия с пользователем')
 @allure.title('Изменение данных пользователя')
 def test_update_user():
-    token = Reqres().get_token(LOGIN, PASSWORD)
+    token = Reqres().get_token(login, password)
     result = Reqres().update_user(user_id=2, name='Anna Zukowska', job='QA', token=token)
     UpdatedUser(result).assert_('Anna Zukowska', 'QA')
 
@@ -61,7 +61,7 @@ def test_update_user():
 @allure.story('Действия с пользователем')
 @allure.title('Удаление пользователя')
 def test_delete_user():
-    token = Reqres().get_token(LOGIN, PASSWORD)
+    token = Reqres().get_token(login, password)
     result = Reqres().delete_user(user_id=2, token=token)
     DeletedUser(result).assert_()
 
